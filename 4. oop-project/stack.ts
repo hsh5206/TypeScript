f10ddd071ccaf2e877c434defb7905bab39bdd21
hsh5206 <about5206@naver.com>
2022-02-20 22:56:17 +0900

    ch07 api

--- 4. oop-project/stack.ts
+++ 4. oop-project/stack.ts
@@ -1,45 +1,47 @@
-interface Stack {
-  readonly size: number;
-  push(value: string): void;
-  pop(): string;
-}
-
-type StackNode = {
-  readonly value: string;
-  readonly next?: StackNode;
-};
-
-class StackImpl implements Stack {
-  private _size: number = 0;
-  private head?: StackNode;
-
-  constructor(private capacity: number) {}
-
-  get size() {
-    return this._size;
+{
+  interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop(): string;
   }
 
-  push(value: string) {
-    if (this.size === this.capacity) {
-      throw new Error("Stack is full");
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+  };
+
+  class StackImpl implements Stack {
+    private _size: number = 0;
+    private head?: StackNode;
+
+    constructor(private capacity: number) {}
+
+    get size() {
+      return this._size;
     }
-    const node: StackNode = { value, next: this.head };
-    this.head = node;
-    this._size++;
-  }
 
-  pop(): string {
-    if (this.head == null) {
-      throw new Error("Stack is empty");
+    push(value: string) {
+      if (this.size === this.capacity) {
+        throw new Error("Stack is full");
+      }
+      const node: StackNode = { value, next: this.head };
+      this.head = node;
+      this._size++;
     }
-    const node = this.head;
-    this.head = node.next;
-    this._size--;
-    return node.value;
-  }
-}
 
-const stack = new StackImpl(10);
-stack.push("a");
-stack.push("b");
-console.log(stack.pop());
+    pop(): string {
+      if (this.head == null) {
+        throw new Error("Stack is empty");
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
+  }
+
+  const stack = new StackImpl(10);
+  stack.push("a");
+  stack.push("b");
+  console.log(stack.pop());
+}
