13162d36cd423d3f22c157aca381cefe700e8579
hsh5206 <about5206@naver.com>
2022-03-11 22:22:57 +0900

    project (3)

--- project/src/app.ts
+++ project/src/app.ts
@@ -1,15 +1,20 @@
-import { Component } from "./components/component";
+import { InputDialog } from "./components/dialog/dialog.js";
+import { Component } from "./components/component.js";
 import { VideoComponent } from "./components/page/item/video.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { ImageComponent } from "./components/page/item/image.js";
-import { Composable, PageComponent } from "./components/page/page.js";
+import {
+  Composable,
+  PageComponent,
+  PageItemComponent,
+} from "./components/page/page.js";
 
 class App {
   private readonly page: Component & Composable;
 
   constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent(
@@ -29,6 +34,19 @@ class App {
 
     const todo = new TodoComponent("Todo Title", "Todo CheckBox");
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
+    imageBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 page에 추가
+        dialog.removeFrom(document.body);
+      });
+      dialog.attachTo(document.body);
+    });
   }
 }
 
